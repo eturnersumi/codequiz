783e25d1f889d157cbcc2cc80552a9be7de7b0d9
Impactful Actions <eturner.et3@outlook.com>
2020-01-04 22:31:41 -0800

    bug fixed where all questions displayed at once

--- questions.js
+++ questions.js
@@ -1,29 +1,54 @@
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      "title" : "Commonly used data types DO NOT include:",
+      "choices" : [
+                    "strings", 
+                    " booleans", 
+                    " alerts", 
+                    " numbers"
+                ],
+      "answer" : "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      "title" : "The condition in an if / else statement is enclosed within ____.",
+      "choices" : [
+                    "quotes", 
+                    " curly brackets", 
+                    " parentheses", 
+                    " square brackets"
+                ],
+      "answer" : "parentheses"
     },
     {
-      title: "Areas of the box model inside out.",
-      choices: ["Padding, Border, Margin, Content", "Border, Margin, Content, Padding", "Content, Padding, Border, Margin", "Content, Border, Padding, Margin"],
-      answer: "Content, Padding, Border, Margin"
+      "title" : "Innermost part of box model",
+      "choices" : [
+                    "Padding", 
+                    " Border", 
+                    " Content", 
+                    " Margin"
+                ],
+      "answer" : "Content"
     },
   
     {
-      title: "Self-closing tags DO NOT include:",
-      choices: ["<img>", "<script>", "<link>", "<iframe>"],
-      answer: "<script>"
+      "title" : "Self-closing tags DO NOT include:",
+      "choices" : [
+                    "<img>", 
+                    " <script>", 
+                    " <link>", 
+                    " <iframe>"
+                ],
+      "answer" : "<script>"
     },
     {
-      title: "Node properties to navigate between nodes with JavaScript DO NOT include:",
-      choices: ["firstChild", "previousSibling", "nextChild", "nextSibling"],
-      answer: "nextChild"
+      "title" : "Node properties to navigate between nodes with JavaScript DO NOT include:",
+      "choices" : [
+                    "firstChild", 
+                    " previousSibling", 
+                    " nextChild", 
+                    " nextSibling"
+                ],
+      "answer" : "nextChild"
     },
 
 ];
