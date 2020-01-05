32c776d239b363f3d4554889a53e0e72b0e62e24
Impactful Actions <eturner.et3@outlook.com>
2020-01-04 20:09:44 -0800

    non-working highscores link added. displaying 1 question w/respective answer. Next question button added. onclick event for answer choices not working

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Areas of the box model inside out.",
+      choices: ["Padding, Border, Margin, Content", "Border, Margin, Content, Padding", "Content, Padding, Border, Margin", "Content, Border, Padding, Margin"],
+      answer: "Content, Padding, Border, Margin"
+    },
+  
+    {
+      title: "Self-closing tags DO NOT include:",
+      choices: ["<img>", "<script>", "<link>", "<iframe>"],
+      answer: "<script>"
+    },
+    {
+      title: "Node properties to navigate between nodes with JavaScript DO NOT include:",
+      choices: ["firstChild", "previousSibling", "nextChild", "nextSibling"],
+      answer: "nextChild"
+    },
+
+];
